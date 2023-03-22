9aa88daf3f114ab564480507458ce03e820a3914
DJIETCHEU NOELLE <noelledjietcheu@gmail.com>
2023-03-22 22:46:46 +0100

    hh

--- src/Component/Login/Login.js
+++ src/Component/Login/Login.js
@@ -1,15 +1,18 @@
 import React, { Component } from "react";
+import TabsComponentNavBar from "../NavBAR/NavBar";
 import { Button } from 'antd';
 import 'antd/dist/reset.css';
 import './Login.css';
 import { Input } from 'antd';
+import NavBAR from "../NavBAR/NavBar";
 
 class Login extends Component{
     
 
         state={
+            
             username: null,
-            password:null
+            password:null,
         } 
 
     render()
@@ -18,13 +21,16 @@ class Login extends Component{
     
     {
         return(
-            <div >
+           
+            <div className="Box">
+                
                 <div className="Input">
+                     <NavBAR/>
                     <Input placeholder="Basic usage" value={this.state.username} onChange={(event)=>this.setstate({username:event.target.value})}/>
                     <Input placeholder="Basic usage" value={this.state.password} onChange={(event)=>this.setstate({password:event.target.value})}/>
                 </div>
 
-        <Button className="Button" type="primary">Button</Button>
+              <Button className="Button" type="primary">Button</Button>
     </div>
         )
     }
